2780df334f9eaaddc5b9ca75c86c0dcc32e10045
Jesus Guevara <jesusguevara23@gmail.com>
2023-05-31 18:58:01 -0400

    Juego culminado usando html, css y js

--- game.js
+++ game.js
@@ -3,32 +3,53 @@ const game =  canvas.getContext('2d');
 const btnUp= document.querySelector('#up');
 const btnLeft= document.querySelector('#left');
 const btnRight= document.querySelector('#right');
-const btnUpDown= document.querySelector('#down');
+const btnDown= document.querySelector('#down');
+const spanLives= document.querySelector('#lives');
+const spanTime= document.querySelector('#time');
+const spanRecord= document.querySelector('#record');
+const pResult= document.querySelector('#result');
+
 
 let canvasSize;
 let elementsSize;
+let level = 0;
+let lives = 3;
+
+let timeStart;
+let timePlayer;
+let timeInterval;
+
+
 const playerPosition = {
     x: undefined,
     y: undefined
-}
+};
+
+const giftPosition = {
+    x: undefined,
+    y: undefined
+};
+
+let enemyPositions= []; 
 
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
 
 function setCanvasSize(){
     if (window.innerHeight > window.innerWidth){
-        canvasSize = window.innerWidth * 0.8;
+        canvasSize = window.innerWidth * 0.7;
     } else {
-        canvasSize = window.innerHeight * 0.8;
+        canvasSize = window.innerHeight * 0.7;
     }
-      
+    canvasSize = Number(canvasSize.toFixed(0));
     canvas.setAttribute('width',canvasSize);
     canvas.setAttribute('height', canvasSize);
     
     elementsSize = canvasSize / 10;
-
+    playerPosition.x = undefined;
+    playerPosition.y = undefined;
     startGame();
-    };
+};
 
 function startGame() {
     console.log({ canvasSize, elementsSize });
@@ -36,11 +57,25 @@ function startGame() {
     game.font = elementsSize + 'px Verdana';
     game.textAlign = 'end';
 
-    const map = maps[0];
+    const map = maps[level];
+    if(!map){
+        gameWin();
+        return;
+    }
+    if(!timeStart){
+        timeStart = Date.now();
+        timeInterval = setInterval(showTime,100);
+        showRecord();
+
+    }
+
     const mapRows = map.trim().split('\n');
     const mapRowCols = mapRows.map(row => row.trim().split(''));
-    console.log({ map, mapRows, mapRowCols });
+    //console.log({ map, mapRows, mapRowCols });
 
+    showLives();
+
+    enemyPositions = [];
     game.clearRect(0,0, canvasSize, canvasSize);
     mapRowCols.forEach((row, rowI) => {
         row.forEach((col, colI) => {
@@ -55,6 +90,13 @@ function startGame() {
                 playerPosition.y = posY;
             }
            
+        }else if (col == 'I'){
+            giftPosition.x = posX;
+            giftPosition.y = posY;
+        }else if (col == 'X'){
+            enemyPositions.push({
+                x: posX, 
+                y: posY});
         }
 
        game.fillText(emoji,posX,posY);
@@ -64,14 +106,86 @@ function startGame() {
     movePlayer(); 
 };
 function movePlayer(){
+    const giftCollisionX = playerPosition.x.toFixed(3) == giftPosition.x.toFixed(3);
+    const giftCollisionY = playerPosition.y.toFixed(3) == giftPosition.y.toFixed(3);
+    const giftCollision = giftCollisionX && giftCollisionY;
+
+
+    if(giftCollision){
+        levelWin();
+    }
+    const enemyCollision = enemyPositions.find(enemy => {
+        const enemyCollisionX = enemy.x.toFixed(3) == playerPosition.x.toFixed(3);
+        const enemyCollisionY = enemy.y.toFixed(3) == playerPosition.y.toFixed(3);
+        return enemyCollisionX && enemyCollisionY;
+    });
+    if(enemyCollision){
+        levelFail();
+    }
+
+
+
     game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y); 
-}
+};
+
+function levelWin(){
+    level++;
+    startGame();
+};
+
+function levelFail(){
+lives--;
+    if (lives <= 0){
+        level = 0;
+        lives = 3;
+        timeStart = undefined;
+        
+    }
+playerPosition.x = undefined;
+playerPosition.y = undefined;
+startGame();
+};
+
+function gameWin(){
+    console.log("terminaste el juego");
+    clearInterval(timeInterval);
+    const recordTime = localStorage.getItem('record_time');
+    const playerTime = Date.now() - timeStart;
+    if (recordTime){
+        
+        if(recordTime >= playerTime){
+            localStorage.setItem('record_time', playerTime);
+            pResult.innerHTML = 'SUPERASTE EL RECORD :)';
+        }else {
+            pResult.innerHTML = 'LO SIENTO, NO SUPERASTE EL RECORD';
+        }
+    } else {
+        localStorage.setItem('record_time', playerTime);
+        pResult.innerHTML = 'INTENTALO DE NUEVO PARA SUPERAR EL TIEMPO ESTABLECIDO Y MARCAR UN NUEVO RECORD :)' ;
+    }
+    console.log({recordTime, playerTime});
+};
+
+function showLives(){
+    const heartsArray = Array(lives).fill(emojis['HEART']);
+    spanLives.innerHTML = "";
+    heartsArray.forEach(heart => spanLives.append(heart));
+};
+
+function showTime(){
+    spanTime.innerHTML = Date.now() - timeStart;
+};
+
+function showRecord(){
+    spanRecord.innerHTML = localStorage.getItem('record_time');
+};
+
 
 window.addEventListener('keydown', moveByKeys);
 btnUp.addEventListener('click', moveUp);
 btnLeft.addEventListener('click', moveLeft);
 btnRight.addEventListener('click', moveRight);
-btnUpDown.addEventListener('click', moveDown);
+btnDown.addEventListener('click', moveDown);
 
 
 
@@ -81,7 +195,7 @@ function moveByKeys(event){
     else if(event.key == 'ArrowRight') moveRight();
     else if(event.key == 'ArrowDown') moveDown();
     
-}
+};
 function moveUp(){
     if((playerPosition.y - elementsSize)<elementsSize){
    
@@ -89,7 +203,7 @@ function moveUp(){
     playerPosition.y -= elementsSize;
     startGame ();
     }
-}
+};
 function moveLeft(){
     if((playerPosition.x - elementsSize)<elementsSize){
    
@@ -97,7 +211,7 @@ function moveLeft(){
         playerPosition.x -= elementsSize;
         startGame ();
         }   
-}
+};
 function moveRight(){
     if((playerPosition.x + elementsSize)>canvasSize){
    
@@ -105,7 +219,7 @@ function moveRight(){
         playerPosition.x += elementsSize;
         startGame ();
         }   
-}
+};
 function moveDown(){
     if((playerPosition.y + elementsSize)>canvasSize){
    
@@ -113,6 +227,6 @@ function moveDown(){
         playerPosition.y += elementsSize;
         startGame ();
         }   
-}
+};
 
 
